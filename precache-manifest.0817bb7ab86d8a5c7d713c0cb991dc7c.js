64b50701d520b0e4a935110ee91d4d4a823da58f
Viorel Craescu <viorel@craescu.com>
2019-04-28 08:03:43 +0300

    Updates

---
+++ precache-manifest.0817bb7ab86d8a5c7d713c0cb991dc7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/chat-firebase/robots.txt"
+  },
+  {
+    "revision": "ad8d99e212ab04b48f13",
+    "url": "/chat-firebase/js/chunk-vendors.22e56186.js"
+  },
+  {
+    "revision": "4462365184c3dd76fe43",
+    "url": "/chat-firebase/js/app.f7374de4.js"
+  },
+  {
+    "revision": "7b99b954dae5e0db44ba67f31092ee9a",
+    "url": "/chat-firebase/index.html"
+  },
+  {
+    "revision": "4462365184c3dd76fe43",
+    "url": "/chat-firebase/css/app.8596aeb0.css"
+  }
+];
